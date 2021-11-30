1251701d7f86497f85513fa52446e20e12170ef0
Clarissa Regita <clarissaregita@gmail.com>
2021-11-30 12:08:46 +0700

    edit"

--- app/src/main/java/id/ac/projekmdp/Start_page.java
+++ app/src/main/java/id/ac/projekmdp/Start_page.java
@@ -4,25 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import id.ac.projekmdp.databinding.ActivityStartPageBinding;
+
 public class Start_page extends AppCompatActivity {
 
+    ActivityStartPageBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+        binding = ActivityStartPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" ));
+        binding.videoView.start();
         Thread welcomeThread = new Thread() {
 
             @Override
             public void run() {
                 try {
                     super.run();
-                    sleep(3000);  //Delay of 10 seconds
+                    sleep(2000);  //Delay of 2 seconds
                 } catch (Exception e) {
 
                 } finally {
