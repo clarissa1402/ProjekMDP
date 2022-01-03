599a3d3479149d2797db6eca030c1f2ec2f886ff
HaniLesmana <hanilesmana01@Gmail.com>
2022-01-03 10:45:18 +0700

    iya e jiji hebat, heil jiji!

--- app/src/main/java/id/ac/projekmdp/Fragment_topup_user.java
+++ app/src/main/java/id/ac/projekmdp/Fragment_topup_user.java
@@ -1,34 +1,32 @@
 package id.ac.projekmdp;
 
-import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.midtrans.sdk.corekit.callback.TransactionFinishedCallback;
+import com.midtrans.sdk.corekit.core.MidtransSDK;
+import com.midtrans.sdk.corekit.core.TransactionRequest;
+import com.midtrans.sdk.corekit.core.themes.CustomColorTheme;
+import com.midtrans.sdk.corekit.models.snap.TransactionResult;
+import com.midtrans.sdk.uikit.SdkUIFlowBuilder;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import id.ac.projekmdp.adapter.homeuseradapter;
-import id.ac.projekmdp.databinding.FragmentHomeUserBinding;
 import id.ac.projekmdp.databinding.FragmentTopupUserBinding;
 import id.ac.projekmdp.kelas.Pegawai;
 import id.ac.projekmdp.kelas.User;
@@ -70,8 +68,8 @@ public class Fragment_topup_user extends Fragment {
         Bundle args = new Bundle();
         //args.putString(ARG_PARAM1, param1);
         //args.putString(ARG_PARAM2, param2);
-        fragment.sedang_login = sedang_login;
         fragment.u=u;
+        fragment.sedang_login = sedang_login;
         fragment.setArguments(args);
         return fragment;
     }
@@ -98,5 +96,26 @@ public class Fragment_topup_user extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         binding.uang.setText("IDR "+sedang_login.getSaldo());
+    }
+    private void makePayment(){
+        SdkUIFlowBuilder.init()
+                .setClientKey("SB-Mid-client-HVTcgsrWFrzF3AVC") // client_key is mandatory
+                .setContext(u.getBaseContext()) // context is mandatory
+                .setTransactionFinishedCallback(new TransactionFinishedCallback() {
+                    @Override
+                    public void onTransactionFinished(TransactionResult result) {
+                        // Handle finished transaction here.
+                    }
+                }) // set transaction finish callback (sdk callback)
+                .setMerchantBaseUrl("https://babowemidtrans.herokuapp.com/index.php/") //set merchant url (required)
+                .enableLog(true) // enable sdk log (optional)
+                .setColorTheme(new CustomColorTheme("#FFE51255", "#B61548", "#FFE51255")) // set theme. it will replace theme on snap theme on MAP ( optional)
+                .buildSDK();
+    }
+    private void clickPay(){
+        String TRANSACTION_ID;
+        int TOTAL_AMOUNT;
+        //TransactionRequest transactionRequest = new TransactionRequest(TRANSACTION_ID, TOTAL_AMOUNT);
+        MidtransSDK.getInstance().startPaymentUiFlow(u.getBaseContext() );
     }
 }
